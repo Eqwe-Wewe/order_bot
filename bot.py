de773dfd035a9c9cfc2bec69b59a4773dd2555b7
samchuk-sv <da_funk_on@mail.ru>
2021-12-12 23:09:43 +0600

    improved FSM

--- bot.py
+++ bot.py
@@ -24,22 +24,40 @@ async def send_message(message):
 
 async def get_data(message):
     chat = message.chat['id']
-    
+
     if chat not in users:
         users[chat] = Order()
 
     order = users[chat]
-    text = await parsing(message.text)
-    if text == 'start':
-        text = order.start()
-        order.choose_size()
-    elif text in ['маленькую', 'маленькая']:
-        text = order.size_selected()
-        order.choose_payment()
+    text_msg = parsing(message.text)
+    if text_msg == 'start':
+        text = order.start_msg()
+        order.start()
+    elif text_msg in ['маленькую', 'маленькая']:
+        order.pizza_size = 'маленькую'
+        text = order.size_selected_msg()
+        order.size_select()
+    elif text_msg in ['большую', 'большая']:
+        order.pizza_size = 'большую'
+        text = order.size_selected_msg()
+        order.size_select()
+    elif text_msg in ['наличкой', 'наличными', 'наликом']:
+        order.payment_method = 'наличкой'
+        text = order.select_payment_method_msg()
+        order.select_payment_method()
+    elif text == 'да':
+        text = order.confirmed_msg()
+        order.confirm()
+        del users[chat]
+    elif text == 'нет':
+        order.not_confirm()
+        text = order.not_confirmed_msg()
+    else:
+        text = order.except_msg()
     return text
 
 
-async def parsing(text):
+def parsing(text):
     return ' '.join(re.findall('\s*(\w+)\s*',text)).lower()
 
 
--- machine.py
+++ machine.py
@@ -5,34 +5,49 @@ class Order():
     def __init__(self):
         self.pizza_size = None
         self.payment_method = None
-        self.states = ['size_selected', 'select_payment_method', 'standby']
+        self.states = [
+            'standby', 'starting', 'selected_size',
+            'selected_payment_method', 'confirmed'
+        ]
         self.transitions = [
-            ['choose_size', 'standby', 'size_selected'],
-            ['error_choose_size', 'standby', 'standby'],
-            ['choose_payment', 'size_selected', 'select_payment_method'],
-            ['error_choose_payment', 'size_selected', 'size_selected'],
-            ['confirm', 'select_payment_method', 'standby'],
-            ['error_confirm', 'select_payment_method', 'select_payment_method'],
-            ['not_confirm', 'select_payment_method', 'size_selected'],
+            ['start', 'standby', 'starting'],
+            ['size_select', 'starting', 'selected_size'],
+            ['select_payment_method', 'selected_size', 'selected_payment_method'],
+            ['confirm', 'selected_payment_method', 'confirmed'],
+            ['not_confirm', 'selected_payment_method', 'standby'],
         ]
         self.machine = Machine(
             self, states=self.states, transitions=self.transitions,
             initial='standby'
         )
 
-    def start(self):
-        return 'Какую пиццу вы хотите заказать?'
+    def start_msg(self):
+        return 'Какую пиццу вы хотите пиццу? Большую или маленькую?'
 
-    def size_selected(self):
+    def size_selected_msg(self):
         return 'Как вы будете платить?'
 
-    def select_payment_method(self, size, method):
+    def select_payment_method_msg(self):
         return (
-            f'вы хотите {self.pizza_size} пиццу, '
+            f'Вы хотите {self.pizza_size} пиццу, '
             f'оплата - {self.payment_method}?'
         )
-    def confirmed(self):
-        return 'Спасибо за заказ!'
+    def confirmed_msg(self):
+        return 'Спасибо за заказ'
+    
+    def not_confirmed_msg(self):
+        return 'Заказ отменен'
+
+    def except_msg(self):
+        if self.is_standby():
+            text = 'Нажмите /start'
+        if self.is_starting():
+            text = 'Можно заказать только маленькую или большую пиццы'
+        if self.is_selected_size():
+            text = 'Доступна оплата только наличными'
+        if self.is_selected_payment_method():
+            text = 'Подтвердить заказ - да\nОтменить заказ - нет'
+        return text
 
 
 if __name__ == '__main__':
